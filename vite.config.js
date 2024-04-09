341774488b9add3451f5d48a65422c949b069976
Jaelia09 <jjmaster0999@gmail.com>
2024-04-09 10:28:16 -0700

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/eclipto-site/",
+  base: "/eclipto-network/",
   plugins: [react()],
   assetsInclude: ['**/*.glb']
 })
