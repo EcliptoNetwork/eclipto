0a38063d3cfdcf8fe9ca45bbc0b704d43b762742
Jaelia09 <jjmaster0999@gmail.com>
2024-04-09 10:01:39 -0700

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/eclipto-site/"
+  base: "/Eclipto Site/",
   plugins: [react()],
   assetsInclude: ['**/*.glb']
 })
